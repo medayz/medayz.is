2b46bec2ded517e7f65c01f0e871290413c68846
medayz <medayoubz4997@gmail.com>
2020-12-03 08:27:58 +0100

    added prettier config, default colors to global style and fetched github data to static props

--- pages/index.js
+++ pages/index.js
@@ -3,54 +3,75 @@ import Image from "next/image";
 
 const styles = {};
 
-export default function Home() {
-  return (
-    <div className="md:container md:mx-auto bg-gradient-to-r from-emerald-50 to-teal-100 font-mono text-green-600 px-6 dark:bg-gray-900 dark:text-green-200">
-      <Head>
-        <title>medayz</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
+export default function Home({ avatar }) {
+    return (
+        <div className="container mx-auto max-w-2xl p-6">
+            <Head>
+                <title>medayz</title>
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+            </Head>
 
-      <main>
-        <section className={styles.bio}>
-          <div className="mb-2">
-            Hello, my full name is Mohamed Ayoub Zahir! too long right ? 😁
-          </div>
-          <div className="mb-2">
-            Well you can call me Ayoub, and on the internet I just use medayz
-            everywhere as my username!
-          </div>
-          <div className="flex flex-row sm:items-center mb-2">
-            <div className="flex-shrink-0">
-              <Image
-                src="/js.png"
-                alt="JS logo"
-                width={21}
-                height={21}
-                layout="fixed"
-              />
-            </div>
-            <span className="ml-2">I'm a fullstack JS developer</span>
-          </div>
-          <div className="flex flex-row sm:items-center mb-2">
-            <div className="flex-shrink-0">
-              <Image
-                src="/react.png"
-                alt="React logo"
-                width={21}
-                height={21}
-                layout="fixed"
-              />
-            </div>
-            <span className="ml-2">
-              I'm currently interested in JavaScript, React and the technologies
-              of their ecosystem!
-            </span>
-          </div>
-        </section>
-      </main>
+            <main>
+                <section className={styles.bio}>
+                    <img
+                        className="rounded-full border-solid border-2 border-gray-50 my-8"
+                        src={avatar}
+                        alt="my twitter picture"
+                        height={100}
+                        width={100}
+                    />
+                    <p className="mb-2">
+                        Hello, my full name is Mohamed Ayoub Zahir! too long
+                        right ? 😁
+                    </p>
+                    <p className="mb-2">
+                        Well you can call me Ayoub, and on internet I just use
+                        medayz everywhere as my username!
+                    </p>
+                    <p className="flex flex-row sm:items-center mb-2">
+                        <span className="flex sm:items-center flex-shrink-0">
+                            <Image
+                                src="/js.png"
+                                alt="JS logo"
+                                width={24}
+                                height={24}
+                                layout="fixed"
+                            />
+                        </span>
+                        <span className="ml-2">
+                            I'm a fullstack JS developer
+                        </span>
+                    </p>
+                    <p className="flex flex-row sm:items-center mb-2">
+                        <span className="flex sm:items-center flex-shrink-0">
+                            <Image
+                                src="/react.png"
+                                alt="React logo"
+                                width={24}
+                                height={24}
+                                layout="fixed"
+                            />
+                        </span>
+                        <span className="ml-2">
+                            I'm currently interested in JavaScript, React and
+                            the technologies of their ecosystem!
+                        </span>
+                    </p>
+                </section>
+            </main>
 
-      <footer className={styles.footer}>2020</footer>
-    </div>
-  );
+            <footer className={styles.footer}>2020</footer>
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    const res = await fetch("https://api.github.com/users/medayz");
+    const data = await res.json();
+
+    return {
+        props: {
+            avatar: data.avatar_url,
+        },
+    };
 }
