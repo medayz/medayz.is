68b228412b12491817387f61acb6dc4cfc933081
medayz <medayoub.zahir@gmail.com>
2021-03-22 17:41:37 +0100

    replaced width height attributes for avatar with tailwind classes

--- library/Avatar.js
+++ library/Avatar.js
@@ -1,11 +1,9 @@
 export default function Avatar({ src, alt, height, width }) {
   return (
     <img
-      className="rounded-full border-solid border-2 border-gray-50 my-8 mr-4"
+      className="rounded-full border-solid border-2 border-gray-50 my-8 mr-4 w-28 h-28"
       src={src}
       alt={alt}
-      height={height}
-      width={width}
     />
   );
 }
